5eea22d29d8d2c221fb5782c320225ed28b4ffc5
Farid Attamimi <97972332+Tamimi2022@users.noreply.github.com>
2022-03-25 18:56:44 +0100

    Add files via upload

---
+++ if-elseStatements/script.js
@@ -0,0 +1,26 @@
+// Part 1
+const age = 10;
+if (age >= 18) {
+    console.log("You allowed in");
+}
+else {
+    console.log("You Not allowed!!!");
+}
+
+// Part 2
+const isFemale = false;
+if (isFemale) {
+    console.log("Hey, You!! Welcome");
+}
+else {
+    console.log("Please Go out !!!");
+}
+
+// Part 3
+const driverStatus = "designated";
+if (driverStatus == "designated") {
+    console.log("You ALLOWED to drive");
+}
+else {
+    console.log("You NOT allowed to drive");
+}
